04cbb3dfab0346d2ba3157d66344f2f299596ce0
April Bollinger <63476704+aprilkat@users.noreply.github.com>
2021-11-15 11:33:40 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+Author: April Bollinger
+Date: 11/15/2021
+Project: Chalk it up
+ */
+
+
+
+
+const chalk = require('chalk');
+
+
+// Really good idea if you want to shorten stuff.
+
+
+console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
+console.log(chalk.blue('---------------') + chalk.white('-------------------------------'));
+console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
+console.log(chalk.blue('---------------') + chalk.white('-------------------------------'));
+console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
+console.log(chalk.white('----------------------------------------------'))
+console.log(chalk.blue('Hello world!'));
+
