0d416deb452b5de169411f3816bf2981076d217a
April Bollinger <63476704+aprilkat@users.noreply.github.com>
2021-11-15 12:42:18 -0600

    Added Chalk text effects

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ Project: Chalk it up
 
 
 const chalk = require('chalk');
-
-
-// Really good idea if you want to shorten stuff.
+const chalkAnimation = require('chalk-animation');
 
 
 console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
@@ -18,6 +16,26 @@ console.log(chalk.blue('---------------') + chalk.white('-----------------------
 console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
 console.log(chalk.blue('---------------') + chalk.white('-------------------------------'));
 console.log(chalk.blue('---------------') + chalk.red('-------------------------------'));
-console.log(chalk.white('----------------------------------------------'))
-console.log(chalk.blue('Hello world!'));
+console.log(chalk.white('----------------------------------------------'));
+console.log(chalk.red('----------------------------------------------'));
+console.log(chalk.white('----------------------------------------------'));
+console.log(chalk.red('----------------------------------------------'));
+console.log(chalk.white('----------------------------------------------'));
+console.log(chalk.red('----------------------------------------------'));
+console.log(chalk.white('----------------------------------------------'));
+
+
+
+
+
+console.log('It was a bright and sunny day in the USA.');
+console.log(chalk.yellow('Everyone was enjoying the sunshine and warm weather.'));
+
+
+console.log(chalk.red('Unfortunately, it would not last.'));
+console.log(chalk.red('Unbeknownst to the people of that small town, there was a storm approaching.'));
+
+
+
+
 
